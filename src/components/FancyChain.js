99998ac09de20d0c84567cdbed58e3afb4cc9a59
Hao Chen <sxxach@gmail.com>
2021-11-09 09:03:00 -0800

    chain key history

--- src/components/FancyChain.js
+++ src/components/FancyChain.js
@@ -2,10 +2,12 @@
 //     chainType: ChainType
 //     chainKey: { key: T; counter: number }
 //     messageKeys: { [key: number]: T }
+//     chainKeyHistory: Array<T>; 
 // }
 
 import React, {useState} from "react"
 import Key from "./Key.js"
+import Button from '@material-ui/core/Button'
 
 export default function FancyChain(props) {
     const [showMessageKeys, setShowMessageKeys] = useState(false)
@@ -15,7 +17,13 @@ export default function FancyChain(props) {
     return(
         <>
             <h2>Chain Info</h2>
-            <Key desc={"chain key"} keyArray = {props.chain.chainKey.key} onclick={() => handleOnClick()}/>
+            <Button onClick={() => {
+                handleOnClick(); 
+                console.log("clicked toggle message key, length = " + Object.keys(props.chain.messageKeys).length)
+            }}>Toggle message keys</Button>
+            {props.chain.chainKeyHistory.map((item, index) => (
+                <Key key={index} desc={"ck" + index} keyArray = {item}/>
+            ))}
             {showMessageKeys && Object.keys(props.chain.messageKeys).map(id => (
                 <Key key={id} desc={"message key"} keyArray = {props.chain.messageKeys[id]}/>
             ))}
